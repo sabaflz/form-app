8a84a843687abd4be12554e050ce6b08ec114f0e
Saba <saba.feilizadeh@gmail.com>
2022-11-15 12:22:22 -0800

    Add first name and last name after Hello.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,53 @@
-import React from "react";
+import React , {useState} from "react";
 
 function App() {
+
+    const [fName, setFName] = useState("");
+    const [lName, setLName] = useState("");
+
+    function updateFName(event){
+        const newFName = event.target.value;
+        setFName(newFName);
+    }
+
+    function updateLName(event){
+        const newLName = event.target.value;
+        setLName(newLName);
+    }
+
+    // changing the color of the submit button to balck on mouse over
+
+    const [color, setColor] = useState("");
+
+    function changeColorToBlack(){
+        setColor("black");
+        document.querySelector("button").style.backgroundColor = color;
+    }
+
+    function changeColorToWhite(){
+        setColor("white");
+        document.querySelector("button").style.backgroundColor = color;
+    }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>Hello {fName} {lName}</h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <button>Submit</button>
+        <input 
+            name="fName"
+            onChange={updateFName}
+            placeholder="First Name"
+            value={fName}
+        />
+        <input 
+            name="lName"
+            onChange={updateLName}
+            placeholder="Last Name"
+            value={lName}
+        />
+        <button 
+            onMouseOver={changeColorToBlack}
+            onMouseOut={changeColorToWhite}>Submit</button>
       </form>
     </div>
   );
